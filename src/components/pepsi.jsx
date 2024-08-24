688bf23a4382c01b33b42ccd5effd63acd588ed9
HarisHassan55 <harishassan551@gmail.com>
2024-08-24 20:24:43 +0500

    first commit

--- src/components/pepsi.jsx
+++ src/components/pepsi.jsx
@@ -38,7 +38,7 @@ const SimpleModel = () => {
   }, [scene]);
 
   return (
-    <Canvas className='basis-3/4 px-[2rem] lg:px-0'>
+    <Canvas className='basis-3/4 lg:basis-4/5 px-[2rem] lg:px-0'>
       <PerspectiveCamera makeDefault position={cameraPosition} fov={75} />
       <ambientLight intensity={2} />
       <directionalLight position={[-5, 1, 3]} intensity={4} />
--- src/components/topleft.jsx
+++ src/components/topleft.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Topleft = () => {
   return (
-    <div className='lg:flex lg:flex-col collapse lg:visible content-center basis-1/4 px-4'>
-      <p className='text-xl leading-loose w-max nerko-one-regular text-center'> why not<br/> <b>MERI JAAN</b> </p>
+    <div className='flex-col collapse lg:visible content-center lg:basis-1/4 px-4'>
+      <p className='text-xs lg:text-6xl leading-loose nerko-one-regular text-start'>why not</p>
+      <p className='text-xs lg:text-6x leading-loose w-[16rem] nerko-one-regular text-start'>MERI JAAN</p>
     </div>
   );
 }
