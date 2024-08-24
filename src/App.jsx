e5c13ac0bbb024916a10fd95013a10e662fba7b6
HarisHassan55 <harishassan551@gmail.com>
2024-08-24 20:25:45 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="flex flex-col relative">
       <HS/>
-      <img className='w-[8rem] md:w-[12rem] absolute top-1 left-1' src={pepsilogo} alt="" />
+      <img className='w-[8rem] md:w-[12rem] absolute top-1 left-0' src={pepsilogo} alt="" />
     </div>
   );
 };
